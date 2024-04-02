455fa2bdb44821925bac80194b9f45e0fbd27408
Mudit Jain <97677133+Mudit-Jxin7@users.noreply.github.com>
2024-04-02 12:15:25 +0530

    Create mod.cpp

---
+++ #include <bits/mod.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+int mod_mul(int a, int b) { return (a * b) % MOD; }
+int mod_add(int a, int b) { return (a + b) % MOD; }
+int mod_sub(int a, int b) { return (a - b) % MOD; }
+int mod_div(int a, int b) { return (a * pow(b , m - 2)) % MOD}
+
+int main()
+{
+
+    return 0;
+}
